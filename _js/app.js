aa828f071eb31dad669d3b5a5abd92a5fdd19104
ZxFrankxZ <frabcrevi16@gmail.com>
2021-11-30 23:44:15 +0100

    .

--- _js/app.js
+++ _js/app.js
@@ -69,7 +69,7 @@ function fadeOutPreloader(element, duration) {
 var TxtRotate = function(el, toRotate, period) {
   this.toRotate = toRotate;
   this.el = el;
-  this.loopNum = 0;
+  this.loopNum = 1;
   this.period = parseInt(period, 10) || 2000;
   this.txt = '';
   this.tick();
@@ -79,21 +79,6 @@ var TxtRotate = function(el, toRotate, period) {
 TxtRotate.prototype.tick = function() {
   var i = this.loopNum % this.toRotate.length;
   var fullTxt = this.toRotate[i];
-  
-  this.txt = fullTxt.substring(0, this.txt.length + 1);
-  this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
-  var that = this;
-  var delta = 200 - Math.random() * 100;
-  
-  setTimeout(function() {
-    that.tick();
-  }, delta);
-};
-
-/*TxtRotate.prototype.tick = function() {
-  var i = this.loopNum % this.toRotate.length;
-  var fullTxt = this.toRotate[i];
-  
 
   if (this.isDeleting) {
     this.txt = fullTxt.substring(0, this.txt.length - 1);
@@ -121,7 +106,7 @@ TxtRotate.prototype.tick = function() {
   setTimeout(function() {
     that.tick();
   }, delta);
-};*/
+};
 
 /* Word Cloud */
 
